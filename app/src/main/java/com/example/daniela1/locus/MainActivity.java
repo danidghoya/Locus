80961ff598af597ca21fdafd078f0918c32d6f99
danidghoya <dD15sd23Ed30@>
2016-11-05 11:01:35 -0400

    Looking activity and renting activity added

--- app/src/main/java/com/example/daniela1/locus/MainActivity.java
+++ app/src/main/java/com/example/daniela1/locus/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.daniela1.locus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void lookingButtonGotPressed(View view)
+    {
+        Intent lookingIntent = new Intent(this, lookingActivity.class);
+        startActivity(lookingIntent);
+    }
+    public void rentingButtonGotPressed (View view)
+    {
+        Intent rentingIntent = new Intent(this, RentersActivity.class);
+        startActivity(rentingIntent);
     }
 }
