c9171f58b9c3697bb0735c60c442f7f8bc6860ba
Eunice Wang <eunicewang@Eunices-MacBook-Pro.local>
2016-11-05 16:56:53 -0400

    settings added

---
+++ app/src/main/java/com/example/daniela1/locus/SettingsActivity.java
@@ -0,0 +1,23 @@
+package com.example.daniela1.locus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    public void signOutButtonGotPressed (View view)
+    {
+        Intent signOutIntent = new Intent(this, IntroActivity.class);
+        startActivity(signOutIntent);
+    }
+
+
+}
