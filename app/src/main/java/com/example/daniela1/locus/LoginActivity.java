f6b2cc7dfb9bdb4badcbb1b266f42ee736961b9f
danidghoya <dD15sd23Ed30@>
2016-11-05 15:12:06 -0400

    Changes to run

--- app/src/main/java/com/example/daniela1/locus/LoginActivity.java
+++ app/src/main/java/com/example/daniela1/locus/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.daniela1.locus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,5 +11,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void loginButtonGotPressed (View view)
+    {
+        Intent loginIntent = new Intent(this, MainActivity.class);
+        startActivity(loginIntent);
+    }
+
+    public void cancelButtonGotPressed (View view)
+    {
+        Intent cancelIntent = new Intent(this, IntroActivity.class);
+        startActivity(cancelIntent);
     }
 }
