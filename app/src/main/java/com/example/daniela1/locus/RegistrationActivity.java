05b2438f0a114c83ff16338954df13ee560c9cb5
Eunice Wang <eunicewang@Eunices-MacBook-Pro.local>
2016-11-06 07:43:46 -0500

    .

--- app/src/main/java/com/example/daniela1/locus/RegistrationActivity.java
+++ app/src/main/java/com/example/daniela1/locus/RegistrationActivity.java
@@ -1,29 +1,147 @@
 package com.example.daniela1.locus;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import static com.example.daniela1.locus.R.id.textView;
+import static com.example.daniela1.locus.R.id.textView2;
+import static com.example.daniela1.locus.R.id.textView3;
+import static com.example.daniela1.locus.R.id.textView4;
+import static com.example.daniela1.locus.R.id.textView5;
+import static com.example.daniela1.locus.R.id.textView6;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void doneButtonGotPressed (View view)
-    {
+    public void doneButtonGotPressed(View view) {
         Intent doneIntent = new Intent(this, MainActivity.class);
         startActivity(doneIntent);
     }
 
-    public void backButtonGotPressed (View view)
-    {
+    public void backButtonGotPressed(View view) {
         Intent backIntent = new Intent(this, IntroActivity.class);
         startActivity(backIntent);
     }
 
-    public void addUserInfo()
+    public void addFirstName(View view) {
+        EditText firstNameEditText = (EditText) findViewById(textView);
+        String sUsername;
+        sUsername = firstNameEditText.getText().toString();
+        if (sUsername.matches("")) {
+            Toast.makeText(this, "You did not enter a first name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    public void addLastName(View view) {
+        EditText lastNameEditText = (EditText) findViewById(textView2);
+        String sUsername;
+        sUsername = lastNameEditText.getText().toString();
+        if (sUsername.matches("")) {
+            Toast.makeText(this, "You did not enter a last name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    public void addUsername(View view) {
+        EditText usernameEditText = (EditText) findViewById(textView3);
+        String sUsername;
+        sUsername = usernameEditText.getText().toString();
+        if (sUsername.matches("")) {
+            Toast.makeText(this, "You did not enter a username", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    public void addPassword(View view) {
+        EditText passwordEditText = (EditText) findViewById(textView4);
+        String sUsername;
+        sUsername = passwordEditText.getText().toString();
+        if (sUsername.matches("")) {
+            Toast.makeText(this, "You did not enter a password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    public void addPhoneNumber(View view) {
+        EditText phoneNumberEditText = (EditText) findViewById(textView5);
+        String sUsername;
+        sUsername = phoneNumberEditText.getText().toString();
+        if (sUsername.matches("")) {
+            Toast.makeText(this, "You did not enter a phone number", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    public void addEmail(View view) {
+        EditText emailEditText = (EditText) findViewById(textView6);
+        String sUsername;
+        sUsername = emailEditText.getText().toString();
+        if (sUsername.matches("")) {
+            Toast.makeText(this, "You did not enter an email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Registration Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
--- app/src/main/java/com/example/daniela1/locus/UserInformations.java
+++ app/src/main/java/com/example/daniela1/locus/UserInformations.java
@@ -7,71 +7,66 @@ import java.util.ArrayList;
  */
 
 
-public class UserInformation {
-
-    public String userFirstName;
-    public String userLastName;
-    public String username;
-    public String password;
-    public String email;
-    public String address;
-    public String phoneNumber;
+public class UserInformations {
 
     ArrayList<ArrayList> contacts = new ArrayList<ArrayList>();
 
-    ArrayList john = new ArrayList();
-    john.add(John);
-    john.add(Smith);
-    john.add(abc123);
-    john.add(qwert12345);
-    john.add(jsmith@gu.edu);
-    john.add(1973 Massachusetts Avenue, Adelphi, Washington DC, 20783);
-    john.add(2028347219);
-
-    contacts.add(john);
-
-    ArrayList david = new ArrayList();
-    david.add(David);
-    david.add(Kim);
-    david.add(hello987);
-    david.add(asdfg13579);
-    david.add(dkim@gmu.edu);
-    david.add(2705 School Street, Washington, Washington DC, 20024);
-    david.add(2029548172);
-
-    contacts.add(david);
-
-    ArrayList rachel = new ArrayList();
-    rachel.add(Rachel);
-    rachel.add(Beck);
-    rachel.add(chris567);
-    rachel.add(zxcvb24680);
-    rachel.add(rbeck@osu.edu);
-    rachel.add(1336 Hickory Lane, Washington, Washington DC, 20005);
-    rachel.add(2024776879);
-
-    contacts.add(rachel);
+    ArrayList<String> john = new ArrayList<String>();
+    ArrayList<String> david = new ArrayList<String>();
+    ArrayList<String> rachel = new ArrayList<String>();
+    ArrayList<String> christine = new ArrayList<String>();
+    ArrayList<String> nick = new ArrayList<String>();
 
 
-    ArrayList christine = new ArrayList();
-    christine.add(Christine);
-    christine.add(Royal);
-    christine.add(kentish135);
-    christine.add(plokij986);
-    christine.add(croyal@cmu.edu);
-    christine.add(3038 Rhode Island Avenue, Herndon, Washington DC, 22070);
-    christine.add(2023864277);
+    public void UserInformation() {
+        john.add("John");
+        john.add("Smith");
+        john.add("abc123");
+        john.add("qwert12345");
+        john.add("jsmith@gu.edu");
+        john.add("1973 Massachusetts Avenue, Adelphi, Washington DC, 20783");
+        john.add("2028347219");
 
-    contacts.add(christine);
+        contacts.add(john);
 
-    ArrayList nick = new ArrayList();
-    nick.add(Nick);
-    nick.add(Everett);
-    nick.add(turnpike999);
-    nick.add(mlnkbj735);
-    nick.add(nevert@uva.edu);
-    nick.add(919 Hickory Lane, Washington, Washington DC, 20036);
-    nick.add(2024636743);
+        david.add("David");
+        david.add("Kim");
+        david.add("hello987");
+        david.add("asdfg13579");
+        david.add("dkim@gmu.edu");
+        david.add("2705 School Street, Washington, Washington DC, 20024");
+        david.add("2029548172");
 
-    contacts.add(nick);
+        contacts.add(david);
+
+        rachel.add("Rachel");
+        rachel.add("Beck");
+        rachel.add("chris567");
+        rachel.add("zxcvb24680");
+        rachel.add("rbeck@osu.edu");
+        rachel.add("1336 Hickory Lane, Washington, Washington DC, 20005");
+        rachel.add("2024776879");
+
+        contacts.add(rachel);
+
+        christine.add("Christine");
+        christine.add("Royal");
+        christine.add("kentish135");
+        christine.add("plokij986");
+        christine.add("croyal@cmu.edu");
+        christine.add("3038 Rhode Island Avenue, Herndon, Washington DC, 22070");
+        christine.add("2023864277");
+
+        contacts.add(christine);
+
+        nick.add("Nick");
+        nick.add("Everett");
+        nick.add("turnpike999");
+        nick.add("mlnkbj735");
+        nick.add("nevert@uva.edu");
+        nick.add("919 Hickory Lane, Washington, Washington DC, 20036");
+        nick.add("2024636743");
+
+        contacts.add(nick);
+    }
 }
