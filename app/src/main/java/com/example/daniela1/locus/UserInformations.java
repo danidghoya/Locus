60f9fa20ca2168b74a0c1eb5ef5fcabf7e31dcf9
Eunice Wang <eunicewang@Eunices-MacBook-Pro.local>
2016-11-06 04:07:57 -0500

    .

--- app/src/main/java/com/example/daniela1/locus/UserInformations.java
+++ app/src/main/java/com/example/daniela1/locus/UserInformations.java
@@ -17,54 +17,61 @@ public class UserInformation {
     public String address;
     public String phoneNumber;
 
-    ArrayList userFirstNames = new ArrayList();
-    ArrayList userLastNames = new ArrayList();
-    ArrayList usernames = new ArrayList();
-    ArrayList passwords = new ArrayList();
-    ArrayList emails = new ArrayList();
-    ArrayList addresses = new ArrayList();
-    ArrayList phoneNumbers = new ArrayList();
+    ArrayList<ArrayList> contacts = new ArrayList<ArrayList>();
 
-    userFirstNames.add(John);
-    userFirstNames.add(David);
-    userFirstNames.add(Rachel);
-    userFirstNames.add(Christine);
-    userFirstNames.add(Nicholas);
+    ArrayList john = new ArrayList();
+    john.add(John);
+    john.add(Smith);
+    john.add(abc123);
+    john.add(qwert12345);
+    john.add(jsmith@gu.edu);
+    john.add(1973 Massachusetts Avenue, Adelphi, Washington DC, 20783);
+    john.add(2028347219);
 
-    userLastNames.add(Smith);
-    userLastNames.add(Kim);
-    userLastNames.add(Beck);
-    userLastNames.add(Royal);
-    userLastNames.add(Everett);
+    contacts.add(john);
 
-    usernames.add(abc123);
-    usernames.add(hello987);
-    usernames.add(chris567);
-    usernames.add(kentish135);
-    usernames.add(turnpike999);
+    ArrayList david = new ArrayList();
+    david.add(David);
+    david.add(Kim);
+    david.add(hello987);
+    david.add(asdfg13579);
+    david.add(dkim@gmu.edu);
+    david.add(2705 School Street, Washington, Washington DC, 20024);
+    david.add(2029548172);
 
-    passwords.add(qwert12345);
-    passwords.add(asdfg13579);
-    passwords.add(zxcvb24680);
-    passwords.add(plokij986);
-    passwords.add(mlnkbj735);
+    contacts.add(david);
 
-    emails.add(jsmith@gu.edu);
-    emails.add(dkim@gmu.edu);
-    emails.add(rbeck@osu.edu);
-    emails.add(croyal@cmu.edu);
-    emails.add(nevert@uva.edu);
+    ArrayList rachel = new ArrayList();
+    rachel.add(Rachel);
+    rachel.add(Beck);
+    rachel.add(chris567);
+    rachel.add(zxcvb24680);
+    rachel.add(rbeck@osu.edu);
+    rachel.add(1336 Hickory Lane, Washington, Washington DC, 20005);
+    rachel.add(2024776879);
 
-    addresses.add(1973 Massachusetts Avenue, Adelphi, Washington DC, 20783);
-    addresses.add(2705 School Street, Washington, Washington DC, 20024);
-    addresses.add(1336 Hickory Lane, Washington, Washington DC, 20005);
-    addresses.add(3038 Rhode Island Avenue, Herndon, Washington DC, 22070);
-    addresses.add(919 Hickory Lane, Washington, Washington DC, 20036);
+    contacts.add(rachel);
 
-    phoneNumbers.add(2028347219);
-    phoneNumbers.add(2029548172);
-    phoneNumbers.add(2024776879);
-    phoneNumbers.add(2023864277);
-    phoneNumbers.add(2024636743);
 
+    ArrayList christine = new ArrayList();
+    christine.add(Christine);
+    christine.add(Royal);
+    christine.add(kentish135);
+    christine.add(plokij986);
+    christine.add(croyal@cmu.edu);
+    christine.add(3038 Rhode Island Avenue, Herndon, Washington DC, 22070);
+    christine.add(2023864277);
+
+    contacts.add(christine);
+
+    ArrayList nick = new ArrayList();
+    nick.add(Nick);
+    nick.add(Everett);
+    nick.add(turnpike999);
+    nick.add(mlnkbj735);
+    nick.add(nevert@uva.edu);
+    nick.add(919 Hickory Lane, Washington, Washington DC, 20036);
+    nick.add(2024636743);
+
+    contacts.add(nick);
 }
