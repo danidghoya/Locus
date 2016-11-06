3b67a3d573e7ddb815bed5d6fc3f24626a638715
Eunice Wang <eunicewang@Eunices-MacBook-Pro.local>
2016-11-06 01:38:09 -0500

    user info added

---
+++ app/src/main/java/com/example/daniela1/locus/UserInformations.java
@@ -0,0 +1,70 @@
+package com.example.daniela1.locus;
+
+import java.util.ArrayList;
+
+/**
+ * Created by eunicewang on 11/6/16.
+ */
+
+
+public class UserInformation {
+
+    public String userFirstName;
+    public String userLastName;
+    public String username;
+    public String password;
+    public String email;
+    public String address;
+    public String phoneNumber;
+
+    ArrayList userFirstNames = new ArrayList();
+    ArrayList userLastNames = new ArrayList();
+    ArrayList usernames = new ArrayList();
+    ArrayList passwords = new ArrayList();
+    ArrayList emails = new ArrayList();
+    ArrayList addresses = new ArrayList();
+    ArrayList phoneNumbers = new ArrayList();
+
+    userFirstNames.add(John);
+    userFirstNames.add(David);
+    userFirstNames.add(Rachel);
+    userFirstNames.add(Christine);
+    userFirstNames.add(Nicholas);
+
+    userLastNames.add(Smith);
+    userLastNames.add(Kim);
+    userLastNames.add(Beck);
+    userLastNames.add(Royal);
+    userLastNames.add(Everett);
+
+    usernames.add(abc123);
+    usernames.add(hello987);
+    usernames.add(chris567);
+    usernames.add(kentish135);
+    usernames.add(turnpike999);
+
+    passwords.add(qwert12345);
+    passwords.add(asdfg13579);
+    passwords.add(zxcvb24680);
+    passwords.add(plokij986);
+    passwords.add(mlnkbj735);
+
+    emails.add(jsmith@gu.edu);
+    emails.add(dkim@gmu.edu);
+    emails.add(rbeck@osu.edu);
+    emails.add(croyal@cmu.edu);
+    emails.add(nevert@uva.edu);
+
+    addresses.add(1973 Massachusetts Avenue, Adelphi, Washington DC, 20783);
+    addresses.add(2705 School Street, Washington, Washington DC, 20024);
+    addresses.add(1336 Hickory Lane, Washington, Washington DC, 20005);
+    addresses.add(3038 Rhode Island Avenue, Herndon, Washington DC, 22070);
+    addresses.add(919 Hickory Lane, Washington, Washington DC, 20036);
+
+    phoneNumbers.add(2028347219);
+    phoneNumbers.add(2029548172);
+    phoneNumbers.add(2024776879);
+    phoneNumbers.add(2023864277);
+    phoneNumbers.add(2024636743);
+
+}
